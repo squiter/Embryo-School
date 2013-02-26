8f6963351dcc59420c4caafb844574ed1afc2e3d
Brunno dos Santos <squiter85@gmail.com>
2013-02-26 15:06:39 -0300

    First configurations

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+gem 'pg'
+gem 'sass-rails',   '~> 3.2.3'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'rspec-rails'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'thin'
+  gem 'better_errors'
+  gem 'meta_request'
+  gem 'binding_of_caller'
+  gem 'pry'
+  gem 'pry-doc'
+  gem 'awesome_print'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Embryo::Application.config.secret_token = 'ad368c8bd244814b312bbf04732b32bea8458d87c171e01fd83fce3a5373a17a807b97e7d9bb5514a7c965fad17ecc0be34f4bc976a029baf974a30b7e65c838'
